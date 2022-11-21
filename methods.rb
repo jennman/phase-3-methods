12e9a0b9c8a05c1b3894a3dc0ed3fbfc9f02ecd8
Jennifer Man <jenniferman702@gmail.com>
2022-11-21 10:44:31 -0500

    commit tree 26a531e2b131e8805a97c2fb8324e3c1f81f9916

--- methods.rb
+++ methods.rb
@@ -1,1 +1,15 @@
 # Your code here!
+def greet_programmer
+    puts "Hello,programmer!"
+
+end
+greet_programmer
+
+def greet name 
+    puts "Hello,#{name}!"
+end
+greet(Jenn)
+
+def greet_with_default(name)
+    puts "Hello,#{name}!"
+end
